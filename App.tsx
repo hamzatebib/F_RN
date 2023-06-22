3c112e30a6e487c78d4495bc18f119a2dcaadab8
Oussama Feki <oussama.feki@docdoku.com>
2023-06-22 17:44:54 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,126 @@
+import { StatusBar } from 'expo-status-bar';
+import { Image,StyleSheet, Text, View } from 'react-native';
+import { LandingScreen } from './src/screens/LandingScreen';
+
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { HomeScreen } from './src/screens/HomeScreen';
+
+
+
+const switchNavigator = createSwitchNavigator({
+  landingStack: {
+    screen: createStackNavigator(
+      {
+        Landing: LandingScreen,
+        // search address screen
+      },
+      {
+        defaultNavigationOptions: {
+          headerShown: false,
+        },
+      }
+    ),
+  },
+
+  homeStack: createBottomTabNavigator({
+    // Home tab Icon
+    home: {
+      screen: createStackNavigator(
+        {
+          HomePage: HomeScreen,
+          //SearchPage: SearchScreen,
+          //RestaurantPage: RestaurantScreen,
+          //FoodDetailPage: FoodDetailScreen,
+        },
+        {
+          defaultNavigationOptions: {
+            headerShown: false,
+          },
+        }
+      ),
+      navigationOptions: {
+        tabBarIcon: ({ focused, tintColor }) => {
+          let icon =
+            focused == true
+              ? require("./src/images/home_icon.png")
+              : require("./src/images/home_n_icon.png");
+          return <Image source={icon} style={styles.tabIcon} />;
+        },
+      },
+    },
+
+    // Home tab Icon
+    Offer: {
+      screen: createStackNavigator({
+        OfferPage: HomeScreen, //
+      }),
+      navigationOptions: {
+        tabBarIcon: ({ focused, tintColor }) => {
+          let icon =
+            focused == true
+              ? require("./src/images/offer_icon.png")
+              : require("./src/images/offer_n_icon.png");
+          return <Image source={icon} style={styles.tabIcon} />;
+        },
+      },
+    },
+
+    // Home tab Icon
+    Cart: {
+      screen: createStackNavigator(
+        {
+          CartPage: HomeScreen,
+          LoginPage: HomeScreen,
+        },
+        {
+          defaultNavigationOptions: {
+            headerShown: false,
+          },
+        }
+      ),
+      navigationOptions: {
+        tabBarIcon: ({ focused, tintColor }) => {
+          let icon =
+            focused == true
+              ? require("./src/images/cart_icon.png")
+              : require("./src/images/cart_n_icon.png");
+          return <Image source={icon} style={styles.tabIcon} />;
+        },
+      },
+    },
+    // Home tab Icon
+    Account: {
+      screen: createStackNavigator({
+        AccountPage: HomeScreen,
+        LoginPage: HomeScreen,
+      }),
+      navigationOptions: {
+        tabBarIcon: ({ focused, tintColor }) => {
+          let icon =
+            focused == true
+              ? require("./src/images/account_icon.png")
+              : require("./src/images/account_n_icon.png");
+          return <Image source={icon} style={styles.tabIcon} />;
+        },
+      },
+    },
+  }),
+});
+
+const AppNavigation = createAppContainer(switchNavigator);
+export default function App() {
+  return (
+
+      <AppNavigation/>
+   
+  );
+}
+
+const styles = StyleSheet.create({
+  tabIcon: {
+    width: 30,
+    height: 30,
+  },
+});
